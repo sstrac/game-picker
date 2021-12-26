9a6f8f284875f981cef2d52374fa1d6ef0254a05
sstrac <s.r.strachan@outlook.com>
2021-12-26 02:29:49 +0000

    titles array created, randomiser button added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap'
 import './App.css';
 
 const API_KEY = process.env.REACT_APP_API_KEY
+const emptyGame = { name: 'Click to get a random game', background_image: 'not found' }
 
-export function API(title) { return `https://api.rawg.io/api/games/${title}?key=${API_KEY}` }
+function API(title) { return `https://api.rawg.io/api/games/${title}?key=${API_KEY}` }
 
-const emptyGame = { name: 'default name', background_image: 'not found' }
+const titles = [
+  'hollow-knight',
+  'grounded'
+]
 
 function App() {
   const [game, setGame] = useState(emptyGame)
+  const [title, setTitle] = useState('hollow-knight')
 
   useEffect(() => {
-    getRAWGData('hollow-knight').then( game => {
+    getRAWGData(title).then( game => {
       console.log(game)
       setGame(game)
     })
     .catch(error => console.log(error))
-  }, [])
+  }, [title])
 
   return (
     <div className={"App"}>
       <Game game={game} setGame={setGame}></Game>
+      <Button onClick={() => setTitle(titles[getRandom(titles.length)])}>Random Game</Button>
     </div>
   );
 }
@@ -38,6 +45,10 @@ async function getRAWGData(title){
   return await fetch(API(title))
   .then(r => r.json())
   .then(resJson =>  resJson )
+}
+
+function getRandom(len){
+  return Math.floor(Math.random() * len)
 }
 
 export default App;
