6be348cfde9a83c063de4c0a69c69d80d4fc5313
sstrac <s.r.strachan@outlook.com>
2021-12-26 02:02:21 +0000

    working api request for hollow knight

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
+export function API(title) { return `https://api.rawg.io/api/games/${title}?key=${API_KEY}` }
+
+const emptyGame = { name: 'default name', background_image: 'not found' }
+
 function App() {
+  const [game, setGame] = useState(emptyGame)
+
+  useEffect(() => {
+    getRAWGData('hollow-knight').then( game => {
+      console.log(game)
+      setGame(game)
+    })
+    .catch(error => console.log(error))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <Game game={game} setGame={setGame}></Game>
     </div>
   );
+}
+
+function Game(props) {
+  return (
+    <div>
+      <h2>{props.game.name}</h2>
+      <img src={props.game.background_image}></img>
+    </div>
+  )
+}
+
+async function getRAWGData(title){
+  return await fetch(API(title))
+  .then(r => r.json())
+  .then(resJson =>  resJson )
 }
 
 export default App;
